b9c0a751f68ca0ef8c56fd9c61006ea086b9d475
Livio Nocchi <livionocchi@gmail.com>
2018-12-15 23:02:00 +0100

    Almost finish

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 let app = express();
 
+app.use('/css', express.static('css'))
+app.use('/img', express.static('img'))
+
 app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/html/pre-home.html')
+})
+
+app.get('/index.html', function(req, res) {
   res.sendFile(__dirname + '/html/index.html')
+})
+
+app.get('/galley.html', function(req, res) {
+  res.sendFile(__dirname + '/html/gallery.html')
+})
+
+app.get('/contact.html', function(req, res) {
+  res.sendFile(__dirname + '/html/contact.html')
 })
 
 app.listen(3000)
