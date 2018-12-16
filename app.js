9f1e384a15a7b04664947284923f1bc262060ad1
Livio Nocchi <livionocchi@gmail.com>
2018-12-16 14:19:40 +0100

    Fixed gallery link. Now it's all set

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ app.use('/css', express.static('css'))
 app.use('/img', express.static('img'))
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/html/pre-home.html')
-})
+  res.sendFile(__dirname + '/html/pre-home.html');
+  console.log(req.query);
+});
 
 app.get('/index.html', function(req, res) {
   res.sendFile(__dirname + '/html/index.html')
-})
+});
 
-app.get('/galley.html', function(req, res) {
+app.get('/gallery.html', function(req, res) {
   res.sendFile(__dirname + '/html/gallery.html')
-})
+});
 
 app.get('/contact.html', function(req, res) {
   res.sendFile(__dirname + '/html/contact.html')
-})
+});
 
 app.listen(3000)
